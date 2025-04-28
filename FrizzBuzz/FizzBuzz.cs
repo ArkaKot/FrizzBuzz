72752e5842e6f09f8b13a76967fb0e99f1cd23d0
Arkadinio <a.kotlowski93@gmail.com>
2025-04-28 08:46:07 +0200

    Dodaj pliki projektów.

---
+++ FrizzBuzz/FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrizzBuzz
+{
+    static class FizzBuzz
+    {
+        public static void CheckNumber(int number)
+        {
+            if (number % 3 == 0 && number % 5 != 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0 && number % 3 != 0) 
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if(number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else
+            {
+                Console.WriteLine($"Wpisana liczba to {number}");
+            }
+
+        }
+    }
+}
---
+++ FrizzBuzz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrizzBuzz
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Podaj liczbę: ");
+                var number = getUserNumber();
+
+                FizzBuzz.CheckNumber(number);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Aby zakończyć naciśnij dowolny klawisz");
+            }
+
+        }
+
+        private static int getUserNumber()
+        {
+            for (; ; )
+            {
+                if (!int.TryParse(Console.ReadLine(), out int userNumber))
+                {
+                    Console.WriteLine("");
+                    continue;
+                }
+                return userNumber;
+            }
+           
+        }
+    }
+}
